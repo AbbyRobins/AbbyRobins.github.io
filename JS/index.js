b539392ac4191ea36b4d550118a2338e30c08cb6
Abby Robins <abigailr2003@hotmail.ca>
2024-11-16 18:32:19 -0700

    feat: add hover effect to alert button
    Add hover effect to alert button above cow

--- JS/index.js
+++ JS/index.js
@@ -27,6 +27,22 @@ document.addEventListener('DOMContentLoaded', function()
 });
 
 //alert button
-document.getElementById('cow-button').addEventListener('click', function() {
+//document.getElementById('cow-button').addEventListener('click', function() {
+//    alert('Boop!');
+//})
+const cowButton = document.getElementById('cow-button');
+
+function changeTextOnHover() {
+    cowButton.setAttribute('data-original-text', cowButton.getAttribute('data-after'));
+    cowButton.setAttribute('data-after', 'Boop me!');
+}
+
+function revertText() {
+    cowButton.setAttribute('data-after', 'Moo!');
+}
+
+cowButton.addEventListener('mouseover', changeTextOnHover);
+cowButton.addEventListener('mouseout', revertText);
+cowButton.addEventListener('click', function() {
     alert('Boop!');
 })
